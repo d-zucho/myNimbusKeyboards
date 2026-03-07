88e05b53962888f43e46a428804872726455ae7c
D_Zucho <danny.nico.dev@outlook.com>
2026-03-07 12:52:40 -0800

    add initial bento box layout and content

--- slices/Hero/index.tsx
+++ slices/Hero/index.tsx
@@ -1,7 +1,10 @@
+"use client";
 import { FC } from "react";
 import { Content } from "@prismicio/client";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import { Bounded } from "@/components/Bounded";
+import { Canvas } from "@react-three/fiber";
+import Scene from "./Scene";
 
 /**
  * Props for `Hero`.
@@ -19,7 +22,9 @@ const Hero: FC<HeroProps> = ({ slice }) => {
       className="blue-gradient-bg relative h-dvh text-white text-shadow-black/30 text-shadow-lg"
     >
       <div className="hero-scene pointer-events-none sticky top-0 h-dvh w-full">
-        {/* CANVAS GOES HERE */}
+        <Canvas shadows="soft">
+          <Scene />
+        </Canvas>
       </div>
       <div className="hero-content absolute inset-x-0 top-0 h-dvh">
         <Bounded
